c63917753f37be5ce9975e288b4d689147e49237
190087 <190087@e0601-17.JC-21.local>
2022-06-08 10:36:19 +0900

    kadai9

--- src/main/java/jp/te4a/spring/boot/myapp9/BookApplication.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/BookApplication.java
@@ -1,11 +1,10 @@
-package jp.te4a.spring.boot.myapp8;
-
+package jp.te4a.spring.boot.myapp9;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(BookApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BookApplication.class, args);
+  }
 }
--- src/test/java/jp/te4a/spring/boot/myapp9/MyBootApp9ApplicationTests.java
+++ src/test/java/jp/te4a/spring/boot/myapp9/MyBootApp9ApplicationTests.java
@@ -1,9 +1,10 @@
-package jp.te4a.spring.boot.myapp8;
+package jp.te4a.spring.boot.myapp9;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookApplicationTests {
+class MyBootApp9ApplicationTests {
 
 	@Test
 	void contextLoads() {
